e92d6e45af1f35abf87230bcc906c291fb110764
Leilani Leach and Manasa Vesala <leilani@>
2019-02-14 14:26:15 -0800

    Added styles.css and imported it in different components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function Header(){
-    var style={
-        textAlign:'center',
+function Header() {
+    var style = {
+        textAlign: 'center',
         fontSize: '18'
     };
-    var styleh1={
-        color:'red',
-        textAlign:'center'
+    var styleh1 = {
+        color: 'darkgray',
+        textAlign: 'center',
+        fontFamily: 'monospace'
     };
     return (
-        <div style={style}> 
+        <div style={style}>
             <h1 style={styleh1}>Famous Programmers and Computer Scientists</h1>
             <h4>Badass women</h4>
             <Link to="/">Home</Link>
